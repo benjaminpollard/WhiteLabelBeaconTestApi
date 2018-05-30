b92639bec7674a6203e7c86b451ef4d54699516c
Benjamin Pollard <Benpollard1990@gmail.com>
2018-05-30 18:47:10 +0900

    add fav con

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
+const favicon = require('express-favicon');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 var routes = require("./routes/routes.js")(app);
+app.use(favicon(__dirname + '/favicon.png'));
 
 var server = app.listen(process.env.PORT || 3000, function () {
     console.log("Listening on port %s...", server.address().port);
